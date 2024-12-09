27202f233f391e05fd09b81d465fbcdea85ebaa0
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-12-09 10:57:34 +0100

    update

--- src/main/java/pl/fullstackdeveloper/payments/application/GetCardsUseCase.java
+++ src/main/java/pl/fullstackdeveloper/payments/application/GetCardsUseCase.java
@@ -2,10 +2,10 @@ package pl.fullstackdeveloper.payments.application;
 
 import pl.fullstackdeveloper.common.PageSpec;
 import pl.fullstackdeveloper.common.ResultPage;
-import pl.fullstackdeveloper.payments.adapters.common.annotations.Adapter;
+import pl.fullstackdeveloper.common.annotations.Atomic;
 import pl.fullstackdeveloper.payments.domain.Card;
 
-@Adapter
+@Atomic
 public class GetCardsUseCase {
 
     private final CardRepository cardRepository;
