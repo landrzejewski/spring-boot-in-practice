5c55328ef6ca3a53263a2cd0a2a15d33e9478a88
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-12-09 16:04:22 +0100

    update

--- payments-api/src/main/java/pl/fullstackdeveloper/payments/AddCardTransactionListenerUseCase.java
+++
@@ -1,9 +0,0 @@
-package pl.fullstackdeveloper.payments;
-
-import java.util.function.Consumer;
-
-public interface AddCardTransactionListenerUseCase {
-
-    void addListener(Consumer<String> listener);
-
-}
