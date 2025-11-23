4b83ff6f6c698ff1c15a6499c867541d2d5192b7
Admin <landrzejewski.poczta@gmail.com>
2025-11-23 19:41:41 +0100

    update

--- src/main/java/pl/fullstackdeveloper/payments/application/AddTransactionUseCase.java
+++ src/main/java/pl/fullstackdeveloper/payments/application/AddTransactionUseCase.java
@@ -27,10 +27,7 @@ public class AddTransactionUseCase {
         var card = findCard(cardNumber);
         var transaction = createTransaction(value, transactionType);
         var cardEventListener = createCardEventListener();
-        card.addEventListener(cardEventListener);
-        card.registerTransaction(transaction);
-        card.removeEventListener(cardEventListener);
-        cardRepository.save(card);
+        addTransactionToCard(card, transaction, cardEventListener);
         return transaction.id();
     }
 
@@ -51,6 +48,13 @@ public class AddTransactionUseCase {
             var applicationEvent = new TransactionAdded(cardNumber, transactionId, transactionType);
             transactionEventPublisher.publish(applicationEvent);
         };
+    }
+
+    private void addTransactionToCard(Card card, Transaction transaction, Consumer<TransactionRegistered> cardEventListener) {
+        card.addEventListener(cardEventListener);
+        card.registerTransaction(transaction);
+        card.removeEventListener(cardEventListener);
+        cardRepository.save(card);
     }
 
 }
