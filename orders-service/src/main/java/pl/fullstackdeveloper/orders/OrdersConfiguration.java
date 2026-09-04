ef39bea139754012a522b51ea356fb118d7f7f47
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-09-04 16:38:55 +0200

    update

--- orders-service/src/main/java/pl/fullstackdeveloper/orders/OrdersConfiguration.java
+++ orders-service/src/main/java/pl/fullstackdeveloper/orders/OrdersConfiguration.java
@@ -1,7 +1,9 @@
 package pl.fullstackdeveloper.orders;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import pl.fullstackdeveloper.orders.application.ConfirmOrderUseCase;
 import pl.fullstackdeveloper.orders.application.Payments;
 import pl.fullstackdeveloper.orders.application.PlaceOrderUseCase;
@@ -17,6 +19,12 @@ public class OrdersConfiguration {
     @Bean
     public ConfirmOrderUseCase confirmOrderUseCase() {
         return new ConfirmOrderUseCase();
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- orders-service/src/main/java/pl/fullstackdeveloper/orders/adapters/PaymentsAdapter.java
+++ orders-service/src/main/java/pl/fullstackdeveloper/orders/adapters/PaymentsAdapter.java
@@ -1,7 +1,10 @@
 package pl.fullstackdeveloper.orders.adapters;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.client.RestTemplate;
 import pl.fullstackdeveloper.common.Money;
 import pl.fullstackdeveloper.orders.application.ConfirmOrderUseCase;
 import pl.fullstackdeveloper.orders.application.PaymentDetails;
@@ -18,17 +21,18 @@ import static pl.fullstackdeveloper.payments.domain.TransactionType.PAYMENT;
 public class PaymentsAdapter implements Payments {
 
     private final ConfirmOrderUseCase confirmOrderUseCase;
+    private final RestTemplate restTemplate;
 
-    public PaymentsAdapter(ConfirmOrderUseCase confirmOrderUseCase) {
+    public PaymentsAdapter(ConfirmOrderUseCase confirmOrderUseCase, RestTemplate restTemplate) {
         this.confirmOrderUseCase = confirmOrderUseCase;
+        this.restTemplate = restTemplate;
     }
 
     @Override
     public String pay(Double amount, Currency currency, PaymentDetails details) {
-        var value = new Money(amount, currency);
-        var cardNumber = new CardNumber(details.cardNumber());
-        // todo
-        return "transactionId.asText();";
+        var paymentRequest = new PaymentRequest(amount, currency.getCurrencyCode(), "OUT");
+        restTemplate.postForObject("http://PAYMENTS/api/cards/0503455605699403/transactions", paymentRequest, Void.class);
+        return "";
     }
 
     @Async
@@ -37,4 +41,7 @@ public class PaymentsAdapter implements Payments {
         confirmOrderUseCase.handle(transactionConfirmed.getTransactionId());
     }
 
+}
+
+record PaymentRequest(Double amount, String currencyCode, String type) {
 }
